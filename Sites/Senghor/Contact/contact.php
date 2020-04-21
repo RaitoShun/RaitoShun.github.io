e3ffa0ed0070eb3b07a88202e3f936fe006db70e
RaitoShun <abrahamthiao@gmail.com>
2020-04-21 15:08:59 +0200

    performance optimisations

--- Sites/Senghor/Contact/contact.php
+++ Sites/Senghor/Contact/contact.php
@@ -34,7 +34,7 @@ $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = " info@senghorbiz.com.pl";//<== update the email address
+$to = " abrahamthiao@gmail.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
