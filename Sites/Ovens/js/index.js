f5632b7e817ab0504ba63e61f9a8828b2b0bf347
RaitoShun <abrahamthiao@gmail.com>
2020-04-21 09:46:11 +0200

    fixing JS logic

--- Sites/Ovens/js/index.js
+++ Sites/Ovens/js/index.js
@@ -18,7 +18,7 @@ var stickyOffset = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scrollFunc() {
-  if (window.pageYOffset >= stickyOffset) {
+  if (window.pageYOffset > stickyOffset) {
     navbar.classList.add("sticky");
     document.body.style.paddingTop = navbar.offsetHeight + 'px';
   } else {
