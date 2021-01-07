d94c1306e2131e9b511e84d4f350c57ef8439200
RaitoShun <abrahamthiao@gmail.com>
2021-01-07 12:52:22 +0200

    test

--- Sites/ISS-Tracker/js/index.js
+++ Sites/ISS-Tracker/js/index.js
@@ -94,7 +94,7 @@ modelLoader.load(
     issMeshPos.add(issModel.scene);
   },
   function (loading) {
-    console.log(loading.loaded / loading.total) / 100;
+    console.log(`${loading.loaded}`);
     if (loading.loaded / loading.total == 1) {
       setTimeout(() => {
         loadScreen.classList.add("invisible");
